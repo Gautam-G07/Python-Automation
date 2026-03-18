97a94ac9620874099e2db36f9bd19fb868f1ea38
Gautam G <gautam26g@gmail.com>
2026-03-18 21:00:44 +0530

    PDF Splitter Tool

--- pdf_tools/pdf_splitter.py
+++ pdf_tools/pdf_splitter.py
@@ -1,13 +1,14 @@
 from PyPDF2 import PdfReader, PdfWriter
 import os
-file_path=r"C:\Users\Gautam G\Python\merged2.pdf"
+file_name=input("Enter the name of the PDF file (with extension): ")
+file_path=os.path.join(r"D:\C Splitter", file_name)
 reader=PdfReader(file_path)
 output_folder=r"D:\C Splitter"
 os.makedirs(output_folder, exist_ok=True)
 for i,page in enumerate(reader.pages):
     splitter = PdfWriter() 
     splitter.add_page(page)
-    output_path=os.path.join(output_folder,f"page_{i+1}.pdf")
+    output_path=os.path.join(output_folder,f"{file_name}_page_{i+1}.pdf")
     with open(output_path,'wb') as output_file:
         splitter.write(output_file)
 print("PDF split into individual pages successfully.")
